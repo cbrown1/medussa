93109edc2c83f6bcc433a516459c93775e9d0aeb
Joseph Kenneth <jranweil@asu.edu>
2010-09-02 15:26:18 -0700

    Reflected changes to `medusa.py` in `__init__.py`.

--- src/medusa/__init__.py
+++ src/medusa/__init__.py
@@ -1,4 +1,4 @@
-from medusa import cmedusa, PaDevice, ContigArrayHandle, ArrayStream, ToneData, ToneStream, generateHostApiInfo, generateDeviceInfo, printAvailableDevices, start_streams
+from medusa import cmedusa, Device, ContigArrayHandle, ArrayStream, ToneData, ToneStream, generateHostApiInfo, generateDeviceInfo, printAvailableDevices, start_streams, open_device, open_default_device
 from portaudio import pa
 
 pa.Pa_Initialize()
