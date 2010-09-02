1d21c83a0cda356e61b0a687102256a4f2f03062
Joseph Kenneth <jranweil@asu.edu>
2010-09-02 09:45:34 -0700

    Made `import medusa` call `Pa_Initialize()`.

--- src/medusa/__init__.py
+++ src/medusa/__init__.py
@@ -1,2 +1,4 @@
 from medusa import cmedusa, PaDevice, ContigArrayHandle, ArrayStream, ToneData, ToneStream, generateHostApiInfo, generateDeviceInfo, printAvailableDevices, start_streams
 from portaudio import pa
+
+pa.Pa_Initialize()
