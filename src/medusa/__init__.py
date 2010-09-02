6d875254fa357b05c8610cb26d7f432db3399d73
Joseph Kenneth <jranweil@asu.edu>
2010-09-02 15:41:53 -0700

    Added `init` call to the `medusa` module `__init__.py`

--- src/medusa/__init__.py
+++ src/medusa/__init__.py
@@ -1,4 +1,4 @@
-from medusa import cmedusa, Device, ContigArrayHandle, ArrayStream, ToneData, ToneStream, generateHostApiInfo, generateDeviceInfo, printAvailableDevices, start_streams, open_device, open_default_device
+from medusa import cmedusa, Device, ContigArrayHandle, ArrayStream, ToneData, ToneStream, generateHostApiInfo, generateDeviceInfo, printAvailableDevices, start_streams, open_device, open_default_device, init
 from portaudio import pa
 
 pa.Pa_Initialize()
