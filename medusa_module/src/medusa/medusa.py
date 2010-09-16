f44523d0feab20725743eee2b042b90bbf6a57c7
Joseph Kenneth <jranweil@asu.edu>
2010-09-16 11:25:21 -0700

    Added ctypes struct subclass for `SF_INFO` struct in libsndfile.

--- medusa_module/src/medusa/medusa.py
+++ medusa_module/src/medusa/medusa.py
@@ -208,6 +208,39 @@ class ArrayStream(Stream):
             self.start()
 
 
+class SF_INFO (ctypes.Structure):
+    """
+    See: http://www.mega-nerd.com/libsndfile/api.html#open
+
+    typedef struct {
+        sf_count_t  frames ;
+        int         samplerate ;
+        int         channels ;
+        int         format ;
+        int         sections ;
+        int         seekable ;
+    } SF_INFO ;
+    """
+    _fields_ = ((frames,     c_uint),
+                (samplerate, c_int),
+                (channels,   c_int),
+                (format,     c_int),
+                (sections,   c_int),
+                (seekable,   c_int))
+
+
+class SndfileStream (Stream):
+    callback = 1
+
+    def __init__(self, device, file_path, scale=1.0, loop=False, samp_format=paFloat32):
+        # open the file
+
+        self.user_data = None
+        self.stream_p = c_void_p()
+        self.device = device
+        self.samp_format = samp_format
+
+
 class ToneStream (Stream):
     callback = 2
 
