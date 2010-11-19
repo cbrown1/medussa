cd65fac25125793440a52af8836daa26e65421d0
unknown <code-breaker@.(none)>
2010-11-18 22:30:39 -0700

    Windows build scripts are now python-version-aware

--- pymedussa/wininstall_py27.bat
+++ pymedussa/wininstall_py27.bat
@@ -2,7 +2,7 @@ REM uninstalling
 msiexec /uninstall dist\medussa-1.0.win32-py2.7.msi /qn
 
 REM Building new installer
-python setup.py bdist_msi
+build_win.bat 2.7
 
 REM Installing
 msiexec /i dist\medussa-1.0.win32-py2.7.msi /quiet
