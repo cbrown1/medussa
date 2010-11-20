8946755b30c6511071e008dabde147bd2fb238db
Christopher Brown <code-breaker@code-breaker-desktop.(none)>
2010-11-20 13:08:10 -0700

    Updated setup.py for linux. windows building now probably broken.

--- libmedussa/build/linux/build.sh
+++ libmedussa/build/linux/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 make
 cp libmedussa.so ../../../pymedussa/lib
-sudo cp libmedussa.so /usr/local/lib
-
--- pymedussa/setup.py
+++ pymedussa/setup.py
@@ -1,42 +1,47 @@
+# -*- coding: utf-8 -*-
 from distutils.core import setup
 import platform
 
 medussa_package = ['medussa']
+medussa_package_dir = 'src/medussa'
 medussa_requires = ['numpy (>=1.2)',]
-medussa_package_data = []
+medussa_package_data = ['*.py']
+medussa_data_files = []
 
 if platform.system() == "Windows":
-    medussa_package_data.append('../../lib/medussa.dll')
-    medussa_package_data.append('../../lib/portaudio_x86.dll')
-    medussa_package_data.append('../../lib/libsndfile-1.dll')
+	medussa_package_data.append('../../lib/medussa.dll')
+	medussa_package_data.append('../../lib/portaudio_x86.dll')
+	medussa_package_data.append('../../lib/libsndfile-1.dll')
 else:
-	medussa_package_data.append('../../lib/libmedussa.so')
+	medussa_data_files.append('lib/libmedussa.so')
 	medussa_requires.append('portaudio (>=19.0)')
 	medussa_requires.append('libsndfile (>=1.0)')
 
 setup(name='medussa',
 	version='1.0',
 	description='Medussa: A cross-platform high-level audio library',
-	author='Joseph K. Ranweiler',
-	author_email='jranweil /at/ asu.edu',
+	author='Christopher A. Brown, Joseph K. Ranweiler',
+	author_email='c-b /at/ asu.edu',
 	maintainer = 'Christopher Brown',
 	maintainer_email = 'c-b /at/ asu.edu',
-	url='',
+	url='http://www.medussa.us',
 	packages = medussa_package,
 	requires = medussa_requires,
-	package_dir={'medussa': 'src/medussa'},
+	package_dir={'medussa': medussa_package_dir},
 	package_data={'medussa': medussa_package_data},
+	data_files=[('lib',medussa_data_files)],
 	keywords = 'sound audio wavplay',
 	license = 'GPL',
 	platforms = 'Win32, Linux',
 	long_description="""\
  Medussa is a cross-platform, high-performance, high-level audio library
- based on Port Audio and libsndfile. You can play NumPy arrays, stream 
- sound files from disk, or create pure tones, square waves, or 'on-line' 
- white or pink noise. There are high-level functions like the matlab-style 
- wavplay function. Medussa also allows you to access specific host api's or 
- devices, create streams as needed, and control them all asynchronously. Or, 
- for the most control, you can access the port audio library directly. Sweet!
+ based on Port Audio and libsndfile. You can play NumPy arrays, stream
+ sound files from disk, or create pure tones, or 'on-line' white or pink
+ noise. There are high-level functions like the playarr function (like
+ matlab's wavplay) function. Medussa also allows you to access specific
+ host api's or devices, create streams as needed, and control them all
+ asynchronously. Or, for the most control, you can access the port audio
+ library directly. Sweet!
  """,
 	classifiers=[
 		"License :: OSI Approved :: GNU General Public License (GPL)",
