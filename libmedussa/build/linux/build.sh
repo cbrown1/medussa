1a7df47ea5a59b88762cef02e44ce6a45cc2296f
Christopher Brown <code-breaker@linux-95y1.site>
2011-03-19 22:51:30 -0700

    Linux build script now takes python version (eg 2.7) as arg

--- libmedussa/build/linux/build.sh
+++ libmedussa/build/linux/build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-make
-cp libmedussa.so ../../../pymedussa/lib
+PYVER=$1
+if [ -z "$PYVER" ]; then
+  PYMAJ=`python -c "import platform; print(platform.python_version_tuple()[0])"`
+  PYMIN=`python -c "import platform; print(platform.python_version_tuple()[1])"`
+  PYVER="${PYMAJ}.${PYMIN}"
+fi
+
+SITEPACKAGES=`python${PYVER} -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
+
+make clean
+make PYVER=${PYVER} FLAGS="-I/usr/include/python${PYVER} -I${SITEPACKAGES}/numpy/core/include/"
+if [ $? -eq 0 ] ; then
+  mkdir ../../../pymedussa/lib/py${PYVER}
+  cp libmedussa.so ../../../pymedussa/lib/py${PYVER}
+else
+  echo "Nope!"
+fi
