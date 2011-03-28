e981f4a6f28722d24439062e295003c94e1e1837
code-breaker <c-b@asu.edu>
2011-03-28 11:35:54 -0700

    package_win.bat now builds meduss.dll.

--- package_win.bat
+++ package_win.bat
@@ -8,6 +8,15 @@ set min=%ver:~2%
 
 rd /s /q build
 
+REM Build lib
+call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+cd lib\build\win\msvc10
+msbuild medussa.sln /property:Configuration="Release Py%ver%"
+cd ..
+mkdir py%ver%
+copy "msvc10\Release Py%ver%\medussa.dll" py%ver% 
+cd ..\..\..
+
 REM Build installer
 c:\python%maj%%min%\python setup.py bdist_msi --plat-name="win32" --target-version="%ver%"
 goto End1
