5d50b8d18f6eb17a70ecca41ab96604d477f1477
Ross Bencina <rossb@audiomulch.com>
2012-04-10 22:05:22 +1000

    Fixed Windows reinstall script to actually install the package after building it (call statement was missing from invocation of package_win.bat). Added diagnostic messages to Windows package build and reinstall scripts.

--- package_win.bat
+++ package_win.bat
@@ -9,11 +9,17 @@ set min=%ver:~2%
 rd /s /q build
 
 REM Build lib
+REM Check and set MedussaVcvarsOk so that we only call vcvarsall.bat once
+if "%MedussaVcvarsOk%"=="1" goto VcvarsOk
 call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 if ERRORLEVEL 1 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+set MedussaVcvarsOk=1
+:VcvarsOk
+
 cd lib\build\win\msvc10
 msbuild medussa.sln /property:Configuration="Release Py%ver%"
 if ERRORLEVEL 1 goto BuildErrorLib
+echo msbuild OK
 cd ..
 mkdir py%ver%
 copy "msvc10\Release Py%ver%\medussa.dll" py%ver% 
@@ -22,6 +28,8 @@ cd ..\..\..
 REM Build installer
 c:\python%maj%%min%\python setup.py bdist_msi --plat-name="win32" --target-version="%ver%"
 if ERRORLEVEL 1 goto BuildErrorInstaller
+echo python setup.py OK
+echo package_win.bat DONE
 goto End1
 
 :BuildErrorLib
@@ -35,3 +43,4 @@ goto End1
 echo Pass Python version number as major.minor. Example: package_win.bat 2.7
 goto End1
 :End1
+echo package_win.bat Leaving
--- reinstall_win.bat
+++ reinstall_win.bat
@@ -1,3 +1,4 @@
+@echo off
 set ver=%1
 
 if %ver%.==. goto NoArg
@@ -7,14 +8,31 @@ set min=%ver:~2%
 
 REM uninstalling
 msiexec /uninstall dist\medussa-1.0.win32-py%ver%.msi /qn
+REM (ignore uninstall errors)
+echo Uninstalling DONE.
 
 REM Building new installer
-package_win.bat %ver%
+call package_win.bat %ver%
+if ERRORLEVEL 1 goto BuildingInstallerError
+echo Building new installer DONE.
 
 REM Installing
 msiexec /i dist\medussa-1.0.win32-py%ver%.msi /quiet
+if ERRORLEVEL 1 goto InstallError
+echo Installing DONE.
+
+goto End1
+
+:BuildingInstallerError
+echo Error building
+goto End1
+
+:InstallError
+echo Error installing
+goto End1
 
 :NoArg
 echo Pass Python version number as major.minor. Example: reinstall_win.bat 2.7
 goto End1
+
 :End1
