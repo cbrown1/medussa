6e53b021be40c58659b89da8dfc59ec331153dc9
Ryan W. Moore <rwmoore07@gmail.com>
2012-12-22 14:27:26 -0500

    Linux Makefile and associated scripts/commands removed due to being unnecessary: setup.py builds the C Medussa library by itself.

--- reinstall_linux.sh
+++ reinstall_linux.sh
@@ -36,17 +36,6 @@ fi
 ./build.sh $pyver
 cd ../../..
 
-installdir=$(python${pyver} -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
-
-if [ -d ${installdir}/medussa ]; then
-    sudo rm -r ${installdir}/medussa
-fi
-if [ -e ${installdir}/medussa-*.egg-info ]; then
-    sudo rm ${installdir}/medussa-*.egg-info
-fi
-if [ -d build ]; then
-    sudo rm -r build
-fi
 python${pyver} setup.py build
 sudo python${pyver} setup.py install
 
