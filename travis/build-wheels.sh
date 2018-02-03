b07490fae932766850253a11efd7a0e59ef139c3
Christopher Brown <cbrown@cbrown1.psylab.pitt.edu>
2018-02-03 13:19:10 -0500

    Try installing numpy again

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,23 +2,23 @@
 set -e -x
 
 # Install system packages required by our library
-#wget http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz && tar -xzvf pa_stable_v19_20140130.tgz && cd portaudio && ./configure && make && make install
-#wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.27.tar.gz && tar -xzvf libsndfile-1.0.27.tar.gz && cd libsndfile-1.0.27 && ./configure && make && make install
+wget http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz && tar -xzvf pa_stable_v19_20140130.tgz && cd portaudio && ./configure && make && make install
+wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.27.tar.gz && tar -xzvf libsndfile-1.0.27.tar.gz && cd libsndfile-1.0.27 && ./configure && make && make install
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-	echo "${PYBIN}"
-#    "${PYBIN}/pip" install -U --only-binary=numpy,scipy numpy scipy
-#    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+	echo "*************************** Building with ${PYBIN}"
+    "${PYBIN}/pip" install -U --only-binary=numpy,scipy numpy scipy pip
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-#for whl in wheelhouse/*.whl; do
-#    auditwheel repair "$whl" -w /io/wheelhouse/
-#done
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
 
 # Install packages and test
-#for PYBIN in /opt/python/*/bin/; do
-#    "${PYBIN}/pip" install medussa --no-index -f /io/wheelhouse
-##    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-#done
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install medussa --no-index -f /io/wheelhouse
+#    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+done
