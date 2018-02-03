a134d9a0f681f829bb17edf2c0b952a99924f191
cbrown1 <cbrown@cbrown1.psylab.pitt.edu>
2018-02-03 18:34:52 -0500

    test that deploy has access to wheels

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,35 +2,43 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+echo "****************************************************"
+echo "****************************************************"
+ls /wheelhouse/manylinux
+echo "****************************************************"
+echo "****************************************************"
 
-    echo "Starting to update gh-pages\n"
+echo "$user"
 
-    #copy data we're interested in to other place
-    cp -R dist $HOME/dist
+# if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
 
-    #go to home and setup git
-    cd $HOME
-    git config --global user.email "cbrown1@pitt.edu"
-    git config --global user.name "cbrown1"
+#     echo "Starting to update gh-pages\n"
 
-    #using token clone gh-pages branch
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
+#     #copy data we're interested in to other place
+#     cp -R dist $HOME/dist
 
-    #go into directory and copy data we're interested in to that directory
-    cd gh-pages
-    cp -Rf $HOME/dist/* .
+#     #go to home and setup git
+#     cd $HOME
+#     git config --global user.email "cbrown1@pitt.edu"
+#     git config --global user.name "cbrown1"
 
-    echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
-    echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
+#     #using token clone gh-pages branch
+#     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
 
-    #add, commit and push files
-    git add -f .
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-    git push -fq origin gh-pages > /dev/null
+#     #go into directory and copy data we're interested in to that directory
+#     cd gh-pages
+#     cp -Rf $HOME/dist/* .
 
-    echo "Done updating gh-pages\n"
+#     echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
+#     echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
 
-else
- echo "Skipped updating gh-pages, because build is not triggered from the master branch."
-fi;
+#     #add, commit and push files
+#     git add -f .
+#     git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+#     git push -fq origin gh-pages > /dev/null
+
+#     echo "Done updating gh-pages\n"
+
+# else
+#  echo "Skipped updating gh-pages, because build is not triggered from the master branch."
+# fi;
