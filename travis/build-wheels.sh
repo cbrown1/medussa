27b1c4b9c8e9c1ad455e7241b960e6bf4eef3ec6
cbrown1 <cbrown@cbrown1.psylab.pitt.edu>
2018-02-03 17:13:06 -0500

    specific pip install twine

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,16 +5,17 @@ set -e -x
 #wget http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz && tar -xzvf pa_stable_v19_20140130.tgz && cd portaudio && ./configure && make && make install
 #wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.27.tar.gz && tar -xzvf libsndfile-1.0.27.tar.gz && cd libsndfile-1.0.27 && ./configure && make && make install
 
-pip install twine
-twine --help
 
 ## Compile wheels
 for PYBIN in /opt/python/*/bin; do
     if [ "$PYBIN" != "/opt/python/cp33-cp33m/bin" ]; then
-        "${PYBIN}/pip" install -U --only-binary=numpy numpy
+        "${PYBIN}/pip" install -U --only-binary=numpy numpy twine
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
     fi
 done
+
+twine --help
+
 #
 #find wheelhouse -name 'numpy*' -exec rm {} \;
 
