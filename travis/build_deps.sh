c2c5330ca040caa0b6fd8925385e20249ba162b0
cbrown1 <cbrown@cbrown1.psylab.pitt.edu>
2018-02-04 21:44:02 -0500

    try removing sudo from build_deps

--- travis/build_deps.sh
+++ travis/build_deps.sh
@@ -3,13 +3,13 @@ tar -xzvf pa_stable_v19_20140130.tgz
 cd portaudio
 ./configure
 make
-sudo make install
+make install
 cd ..
 wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.27.tar.gz
 tar -xzvf libsndfile-1.0.27.tar.gz
 cd libsndfile-1.0.27
 ./configure 
 make
-sudo make install
+make install
 cd ..
 pip install -U --only-binary=numpy numpy
