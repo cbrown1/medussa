991ab4d7e4944788e919976657c52523284099bc
cbrown1 <cbrown@cbrown1.psylab.pitt.edu>
2018-02-04 22:30:17 -0500

    build_deps: try building deps only once

--- travis/build_deps.sh
+++ travis/build_deps.sh
@@ -1,15 +1,18 @@
-wget http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz
-tar -xzvf pa_stable_v19_20140130.tgz
-cd portaudio
-./configure
-make
-make install
-cd ..
-wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.27.tar.gz
-tar -xzvf libsndfile-1.0.27.tar.gz
-cd libsndfile-1.0.27
-./configure 
-make
-make install
-cd ..
+
+if [ ! -d "portaudio" ]; then
+    wget http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz
+    tar -xzvf pa_stable_v19_20140130.tgz
+    cd portaudio
+    ./configure
+    make
+    make install
+    cd ..
+    wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.27.tar.gz
+    tar -xzvf libsndfile-1.0.27.tar.gz
+    cd libsndfile-1.0.27
+    ./configure 
+    make
+    make install
+    cd ..
+fi
 pip install -U --only-binary=numpy numpy
